6a4c473afa1450b424d2e6fdf4b80900bdea21b2
cahighlander <cahighlander>
2001-03-07 20:27:48 +0000

    Initial version

---
+++ common/video/mpeg4/tools/entropy/bytestrm_file.hpp
@@ -0,0 +1,32 @@
+
+#ifndef __BYTESTRM_FILE__
+#define __BYTESTRM_FILE__ 1
+#include "bytestrm.hpp"
+#include <istream.h>
+
+class CInByteStreamFile : public CInByteStreamBase
+{
+ private:
+  istream *m_pInStream;
+  streampos m_bookmark_strmpos;
+  int m_bookmark_eofstate;
+ public:
+  CInByteStreamFile(istream &inStream) { m_pInStream = &inStream; };
+  ~CInByteStreamFile() {};
+  int eof(void) { return (m_pInStream->eof()); };
+  char get(void) { return (m_pInStream->get());};
+  char peek(void) { return (m_pInStream->peek()); };
+  void bookmark (int bSet) {
+    if (bSet) {
+      m_bookmark_strmpos = m_pInStream->tellg();
+      m_bookmark_eofstate = m_pInStream->eof();
+    } else {
+      m_pInStream->seekg(m_bookmark_strmpos);
+      if (m_bookmark_eofstate == 0)
+	m_pInStream->clear();
+    }
+  };
+  void reset(void) { m_pInStream->seekg(0);};
+};
+
+#endif
