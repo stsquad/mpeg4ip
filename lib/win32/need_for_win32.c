2678aa2dd206609137198e444fa0e376e30f3a26
wmaycisco <wmaycisco>
2001-12-15 00:05:49 +0000

    Windoze changes for solaris.

--- lib/win32/need_for_win32.c
+++ lib/win32/need_for_win32.c
@@ -10,24 +10,3 @@ int gettimeofday (struct timeval *t, void *foo)
 	return (0);
 }
 
-char *strsep (char **sptr, const char *delim)
-{
-	char *start, *ret;
-	start = ret = *sptr;
-	if ((ret == NULL) || ret == '\0') {
-	   return (NULL);
-	}
-
-	while (*ret != '\0' &&
-		   strchr(delim, *ret) == NULL) {
-		ret++;
-	}
-	if (*ret == '\0') {
-		*sptr = NULL;
-	} else {
-	    *ret = '\0';
-	    ret++;
-	    *sptr = ret;
-	}
-	return (start);
-}
